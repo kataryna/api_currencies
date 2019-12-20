70237f5cd22765e77ab0e1665ce287b6da1a5998
kataryna <kzurawska@gmail.com>
2019-12-20 01:46:44 +0100

    descriptions and comments

--- rest_countries.py
+++ rest_countries.py
@@ -1,3 +1,8 @@
+"""
+This file contains class to integration with external api https://restcountries.eu/rest/v2
+Using this api, creates text files  with the same name as the three-letter currency name.
+Each file contains a list of countries using this currency
+"""
 import glob
 import os
 import requests
@@ -8,15 +13,20 @@ from json import JSONDecodeError
 class RestCountries():
 
     def __init__(self):
+        """
+        Class implements integration with external api https://restcountries.eu/rest/v2
+        to download list of countries and theirs currencies
+        """
         self.url = 'https://restcountries.eu/rest/v2'
-        self.directory = 'currencies'
+        self.directory_to_write = 'currencies'
         self.prepare_currencies_directory()
 
-    def request(self, method='GET'):
-        response = requests.request(
-            method,
-            url=self.url,
-        )
+    def request(self):
+        """
+        Method sends simple GET request to api, and returns content from its response
+        :return: response content as  json
+        """
+        response = requests.request("GET", url=self.url)
         try:
             content = json.loads(response.content.decode(errors='ignore'))
         except JSONDecodeError:
@@ -25,27 +35,53 @@ class RestCountries():
         return content
 
     def parse_countries(self):
+        """
+        The method retrieves a list of countries from api in json format and parses it to group them by currency
+        creates currency directory with txt files (every currency in one file)
+        :return: None
+        """
         content = self.request()
 
         for country in content:
             self.parse_currencies(country['name'], country['currencies'])
 
-    def parse_currencies(self, country_name: str, country_currencies: dict):
+    def parse_currencies(self, country_name: str, country_currencies):
+        """
+        The method parses a list of country_currencies , for each one  creates text file named {currency_name}.txt (if doesn't exist) .
+        Then appends {country_name} to this file
+        :param country_name: str , name of country
+        :param country_currencies: json object of currencies list
+        :return: None
+        """
         for currency in country_currencies:
-            if currency['code'] == None or currency['code'] == '(none)': #kilka walut nie miało przypisanego kodu waluty
-                if not currency['name']: #gdy nie ma kodu ani nazwy to ignorujemy
+            # some currencies hasn't  currency code assigned, I get a currency name
+            if currency['code'] == None or currency['code'] == '(none)':
+                #some currencies hasn't currency name assigned, I ignore them
+                if not currency['name']:
                     continue
                 currency['code'] = currency['name']
 
             self.add_country_to_currency(country_name,currency['code'])
 
     def add_country_to_currency(self, country_name: str, currency_code: str):
-        with open(f'{self.directory}/{currency_code}.txt','a') as file:
+        """
+        The method appends {country_name} to file {currency_code}.txt (if doesnt exists, creates it)
+        in directory {self.directory_to_write}
+        :param country_name: str
+        :param currency_code: tree-letter currency code
+        :return: None
+        """
+        with open(f'{self.directory_to_write}/{currency_code}.txt','a') as file:
             file.write(country_name+"\n")
 
     def prepare_currencies_directory(self):
-        os.makedirs(self.directory, exist_ok=True) #tworzę katalog, jeśli nie istnieje
-        files = glob.glob(f'{self.directory}/*.txt') #usuwam stare pliki
+        """
+        The method prepares directory {self.directory_to_write}
+        :return: None
+        """
+        os.makedirs(self.directory_to_write, exist_ok=True)
+
+        files = glob.glob(f'{self.directory_to_write}/*.txt')
         for f in files:
             os.remove(f)
 
